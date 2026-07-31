82af7c5fad922e1bc0ca8a5115671064f4ad86b5
Matheus Alves <99508668+Matheus-TestUser1@users.noreply.github.com>
2026-07-31 10:24:41 -0300

    Update MainActivity.kt

--- app/src/main/java/dev/busung/s25uroot/MainActivity.kt
+++ app/src/main/java/dev/busung/s25uroot/MainActivity.kt
@@ -1377,7 +1377,7 @@ private fun SideChoiceMenu(
 
 private const val MENU_EXIT_ANIMATION_MILLIS = 180
 private const val MENU_EXIT_WAIT_MILLIS = 200L
-private const val ROOT_MY_GALAXY_URL = "https://github.com/BuSung-dev/Root-My-Galaxy"
+private const val ROOT_MY_GALAXY_URL = "https://github.com/Matheus-TestUser1/Root-My-Galaxy"
 
 @Composable
 private fun languageLabel(tag: String): String = when {
